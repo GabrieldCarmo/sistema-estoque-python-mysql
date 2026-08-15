61a7a155680e8a6ee165447578088bb38e423f69
Gabriel <gabrieldcarmo1@gmail.com>
2026-08-15 16:45:59 -0300

    13 - Criando o controller de produto

--- controllers/produto_controller.py
+++ controllers/produto_controller.py
@@ -1,9 +1,28 @@
 from models.produto import Produto
-from repositories.categoria_repository import ProdutoRepository
+from repositories.produto_repository import ProdutoRepository
+
 
 class ProdutoController:
+    
+    def __init__(self):
+        self.repository = ProdutoRepository()
 
     def cadastrar(self, nome, preco, quantidade, categoria_id):
         produto = Produto(None, nome, preco, quantidade, categoria_id)
-        return self.repository
+        return self.repository.cadastrar(produto)
         
+    def editar(self, id, nome, preco, quantidade, categoria_id):
+        produto = Produto(id, nome, preco, quantidade, categoria_id)
+        return self.repository.editar(produto)
+    
+    def excluir(self, id):
+        return self.repository.excluir(id)
+    
+    def listar(self):
+        return self.repository.listar()
+    
+    def buscar(self, nome):
+        return self.repository.buscar(nome)
+    
+    def contar(self):
+        return self.repository.contar()
