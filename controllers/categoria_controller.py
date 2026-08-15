75581f58461dba28873e0dc0b4094d3fe485582b
Gabriel <gabrieldcarmo1@gmail.com>
2026-08-15 15:12:37 -0300

    6 - Separando os repositories da forma correta

--- controllers/categoria_controller.py
+++ controllers/categoria_controller.py
@@ -1,51 +1,21 @@
 from models.categoria import Categoria
-from config.database import conectar
+from repositories.categoria_repository import CategoriaRepository
+
 
 class CategoriaController:
 
+    def __init__(self):
+        self.repository = CategoriaRepository()
+
     def cadastrar(self, nome):
         categoria = Categoria(None, nome)
-        conexao = conectar()
-        cursor = conexao.cursor()
-        cursor.callproc("cadastrar_categoria", (categoria.nome,))
-        conexao.commit()
-        cursor.close()
-        conexao.close()
-        return True
-        
+        return self.repository.cadastrar(categoria)
+
     def listar(self):
-        conexao = conectar()
-        cursor = conexao.cursor()           
-        cursor.execute("SELECT * FROM categoria")
-        dados = cursor.fetchall()
-        cursor.close()
-        conexao.close()
-        categorias = []
-        for dado in dados:
-            categoria = Categoria(dado[0], dado[1])
-            categorias.append(categoria)
-        return categorias
+        return self.repository.listar()
 
     def buscar(self, id):
-        conexao = conectar()
-        cursor = conexao.cursor()
-        sql = "SELECT * FROM categoria WHERE id=%s"
-        valores = (id,)
-        cursor.execute(sql, valores)
-        dado = cursor.fetchone()
-        conexao.close()
-        cursor.close()
-        if dado is None:
-            return None
-        categoria = Categoria(dado[0], dado[1])
-        return categoria
+        return self.repository.buscar(id)
 
     def excluir(self, id):
-        conexao = conectar()
-        cursor = conexao.cursor()
-        sql = "DELETE FROM categoria WHERE id=%s"
-        valores = (id,)
-        cursor.execute(sql,valores)
-        conexao.commit()
-        conexao.close()
-        cursor.close()
+        return self.repository.excluir(id)
---
+++ repositories/categoria_repository.py
@@ -0,0 +1,65 @@
+from models.categoria import Categoria
+from config.database import conectar
+
+class CategoriaRepository:
+
+    def cadastrar(self, nome):
+        conexao = conectar()
+        cursor = conexao.cursor()
+
+        cursor.callproc("cadastrar_categoria", (categoria.nome,))
+        
+        conexao.commit()
+        
+        cursor.close()
+        conexao.close()
+        
+        return True
+        
+    def listar(self):
+        conexao = conectar()
+        cursor = conexao.cursor()       
+
+        cursor.execute("SELECT * FROM categoria")
+        dados = cursor.fetchall()
+        
+        cursor.close()
+        conexao.close()
+
+        categorias = []
+
+        for dado in dados:
+            categorias.append(Categoria(dado[0], dado[1]))
+
+        return categorias
+
+    def buscar(self, id):
+        conexao = conectar()
+        cursor = conexao.cursor()
+
+        sql = "SELECT * FROM categoria WHERE id=%s"
+        valores = (id,)
+        cursor.execute(sql, valores)
+
+        dado = cursor.fetchone()
+
+        conexao.close()
+        cursor.close()
+
+        if dado is None:
+            return None
+
+        return Categoria(dado[0], dado[1])  
+
+    def excluir(self, id):
+        conexao = conectar()
+        cursor = conexao.cursor()
+
+        sql = "DELETE FROM categoria WHERE id=%s"
+        valores = (id,)
+        cursor.execute(sql,valores)
+
+        conexao.commit()
+
+        cursor.close()
+        conexao.close()
